e99dc1c2df31ed35ec9a2b14b0dc3e6b50ab7429
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2022-11-08 15:23:42 +0200

    typescript create

--- src/common/ProfilePhoto/ProfilePhoto.js
+++
@@ -1,8 +0,0 @@
-import style from "./ProfilePhoto.module.css"
-let ProfilePhoto = (props) => {
-    return (<div className={style.photo} >
-        <img src={props.photo} />
-        {props.lookingForAJob ? <div className={style.islookingForAJob}>Open to work</div> : null}
-    </div>)
-}
-export default ProfilePhoto
--- src/utils/validator/validators.js
+++
@@ -1,9 +0,0 @@
-export const required = (value) => {
-    if (value) return undefined
-    return "Field is Required "
-}
-
-export const maxLenghtCreator = (maxLenght) => value => {
-    if (value && value.length > maxLenght) return `Max lenght is ${maxLenght} symbols`
-    return undefined
-}
